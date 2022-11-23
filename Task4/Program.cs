95584478124b4b07323905baee56b541b1c8cce5
Solodkov <cartmaness045@gmail.com>
2022-11-23 20:09:48 +0500

    ДЗ по семинару №1

---
+++ Task4/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("Введите число n= ");
+
+int n = Convert.ToInt32(Console.ReadLine());
+
+int numbers = 2;
+
+Console.WriteLine("Чётные числа ");
+
+while (numbers <= n) // в задачи небыло уточнения по поводу N включительно или нет, поэтому включил
+{
+     Console.Write(numbers); Console.Write(", ");
+    numbers = numbers + 2;
+}
+    
